984117c288d439096deed7be2cebc5f670a93497
Pritam Saha <saha7pritam@gmail.com>
2024-02-01 17:31:28 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 const Contact = () => {
     return (
-      <div>
+      <div className="text-center font-semibold p-5 m-5">
         <h1>Thank you for contacting</h1>
         <h2>Now you can go Home</h2>
       </div>
